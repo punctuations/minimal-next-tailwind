96f65b66fcf539d2bb83faf9d47dab9bc1616330
punctuations <matt@dont-ping.me>
2021-03-08 16:55:09 -0800

    start of pricing

---
+++ components/assets/price.jsx
@@ -0,0 +1,16 @@
+const Price = () => {
+	return (
+		<section className="flex flex-col justify-center items-center space-y-5">
+			<br />
+			<header className="flex flex-col justify-center items-center space-y-3">
+				<h1 className="text-4xl font-bold text-gray-900">Pricing Plans</h1>
+				<p className="text-gray-400">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
+					eligendi veritatis tempore.
+				</p>
+			</header>
+		</section>
+	);
+};
+
+export default Price;
---
+++ pages/pricing.js
@@ -0,0 +1,31 @@
+import Head from "next/head";
+import Loader from "../components/assets/loader";
+import Nav from "../components/assets/navigation";
+import Cta from "../components/assets/cta";
+import Footer from "../components/assets/footer";
+import Copyright from "../components/assets/copyright";
+
+import Price from "../components/assets/price";
+
+export default function Pricing() {
+	return (
+		<>
+			<Head>
+				<title>Next - Minimal - Tailwind</title>
+			</Head>
+			<Loader />
+
+			<Nav logo="/favicon.ico" name="Lorem Ipsum" />
+			<div className="mx-12 space-y-8">
+				<div className="flex flex-col justify-center items-center">
+					<Price />
+				</div>
+				<hr />
+				<Cta />
+				<hr />
+				<Footer />
+				<Copyright />
+			</div>
+		</>
+	);
+}
